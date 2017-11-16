5ea88e51f31e3e8a096b5dea768d7ca12d87ba25
Aleksey Ozimkov <aozimkov@gmail.com>
2017-11-16 03:37:22 +0300

    View initialization moves from methods to the top. "Resources res =" has been changed to getResources

--- app/src/main/java/com/example/android/footballscorekeeper/MainActivity.java
+++ app/src/main/java/com/example/android/footballscorekeeper/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.android.footballscorekeeper;
 
-import android.content.res.Resources;
 import android.graphics.Color;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -27,10 +26,30 @@ public class MainActivity extends AppCompatActivity {
     //Display delaying
     int pause = 1500;
 
+    LinearLayout mainDisplay;
+    TextView scoresDisplay;
+    TextView scoresDisplaySecond;
+    Button redButtonA;
+    Button yellowButtonA;
+    Button goalButtonA;
+    Button redButtonB;
+    Button yellowButtonB;
+    Button goalButtonB;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainDisplay = findViewById(R.id.maindisplay); //Main display LinearLayout
+        scoresDisplay = findViewById(R.id.scoresdisplay);
+        scoresDisplaySecond = findViewById(R.id.display_second_string);
+        redButtonA = findViewById(R.id.reda);
+        yellowButtonA = findViewById(R.id.yellowa);
+        goalButtonA = findViewById(R.id.goala);
+        redButtonB = findViewById(R.id.redb);
+        yellowButtonB = findViewById(R.id.yellowb);
+        goalButtonB = findViewById(R.id.goalb);
 
         reset();
     }
@@ -62,45 +81,34 @@ public class MainActivity extends AppCompatActivity {
      */
 
     public void displayDefault(){
-        LinearLayout mainDisplay = findViewById(R.id.maindisplay);
         mainDisplay.setBackgroundColor(Color.DKGRAY);
     }
 
     public void displayRed(){
-        LinearLayout mainDisplay = findViewById(R.id.maindisplay);
         mainDisplay.setBackgroundColor(Color.RED);
-        TextView scoresDisplaySecond = findViewById(R.id.display_second_string);
         scoresDisplaySecond.setText(red.toUpperCase());
     }
 
     public void displayYellow(){
-        LinearLayout mainDisplay = findViewById(R.id.maindisplay);
         mainDisplay.setBackgroundColor(Color.YELLOW);
-        TextView scoresDisplaySecond = findViewById(R.id.display_second_string);
         scoresDisplaySecond.setText(yellow.toUpperCase());
         scoresDisplaySecond.setTextColor(Color.DKGRAY);
-        TextView scoresDisplayMain = findViewById(R.id.scoresdisplay);
-        scoresDisplayMain.setTextColor(Color.DKGRAY);
+        scoresDisplay.setTextColor(Color.DKGRAY);
 
     }
 
     public void displayGoal(){
-        LinearLayout mainDisplay = findViewById(R.id.maindisplay);
         mainDisplay.setBackgroundColor(Color.GREEN);
-        TextView scoresDisplaySecond = findViewById(R.id.display_second_string);
         scoresDisplaySecond.setText(goal.toUpperCase());
         scoresDisplaySecond.setTextColor(Color.DKGRAY);
-        TextView scoresDisplayMain = findViewById(R.id.scoresdisplay);
-        scoresDisplayMain.setTextColor(Color.DKGRAY);
+        scoresDisplay.setTextColor(Color.DKGRAY);
     }
 
     public void displayTeamA(){
-        TextView scoresDisplay = findViewById(R.id.scoresdisplay);
         scoresDisplay.setText(getResources().getText(R.string.teama));
     }
 
     public void displayTeamB(){
-        TextView scoresDisplay = findViewById(R.id.scoresdisplay);
         scoresDisplay.setText(getResources().getText(R.string.teamb));
     }
 
@@ -169,54 +177,42 @@ public class MainActivity extends AppCompatActivity {
      * set a red cards quantity for team A
      */
     public void setRedA(int score){
-        Button redButtonA = findViewById(R.id.reda);
-        Resources res = getResources();
-        redButtonA.setText(String.format(res.getString(R.string.reda), score));
+        redButtonA.setText(String.format(getResources().getString(R.string.reda), score));
     }
 
     /**
      * set a yellow cards quantity for team A
      */
     public void setYellowA(int score){
-        Button redButtonA = findViewById(R.id.yellowa);
-        Resources res = getResources();
-        redButtonA.setText(String.format(res.getString(R.string.yellowa), score));
+        yellowButtonA.setText(String.format(getResources().getString(R.string.yellowa), score));
     }
 
     /**
      * set a goals quantity for team A
      */
     public void setGoalA(int score){
-        Button redButtonA = findViewById(R.id.goala);
-        Resources res = getResources();
-        redButtonA.setText(String.format(res.getString(R.string.goala), score));
+        goalButtonA.setText(String.format(getResources().getString(R.string.goala), score));
     }
 
     /**
      * set a red cards quantity for team B
      */
     public void setRedB(int score){
-        Button redButtonA = findViewById(R.id.redb);
-        Resources res = getResources();
-        redButtonA.setText(String.format(res.getString(R.string.redb), score));
+        redButtonB.setText(String.format(getResources().getString(R.string.redb), score));
     }
 
     /**
      * set a yellow cards quantity for team B
      */
     public void setYellowB(int score){
-        Button redButtonA = findViewById(R.id.yellowb);
-        Resources res = getResources();
-        redButtonA.setText(String.format(res.getString(R.string.yellowb), score));
+        yellowButtonB.setText(String.format(getResources().getString(R.string.yellowb), score));
     }
 
     /**
      * set a goals quantity for team B
      */
     public void setGoalB(int score){
-        Button redButtonA = findViewById(R.id.goalb);
-        Resources res = getResources();
-        redButtonA.setText(String.format(res.getString(R.string.goalb), score));
+        goalButtonB.setText(String.format(getResources().getString(R.string.goalb), score));
     }
 
     /**
@@ -224,11 +220,8 @@ public class MainActivity extends AppCompatActivity {
      */
     public void setDisplay(){
         displayDefault();
-        TextView scoresDisplay = findViewById(R.id.scoresdisplay);
-        Resources res = getResources();
-        scoresDisplay.setText(String.format(res.getString(R.string.scores), scoreTeamA, scoreTeamB));
+        scoresDisplay.setText(String.format(getResources().getString(R.string.scores), scoreTeamA, scoreTeamB));
         scoresDisplay.setTextColor(Color.WHITE);
-        TextView scoresDisplaySecond = findViewById(R.id.display_second_string);
         scoresDisplaySecond.setText(getResources().getText(R.string.scoretext));
         scoresDisplaySecond.setTextColor(Color.WHITE);
     }
